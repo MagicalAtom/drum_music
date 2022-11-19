a9447802706cdc592077e1894371c38c914ad80b
MagicalAtom <magicsoftware.org@gmail.com>
2022-11-19 03:44:51 -0800

    the end

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-
+import 'package:audioplayers/audioplayers.dart';
 void main() {
   var my = MyApp();
   runApp(my);
@@ -9,28 +9,89 @@ class  MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-return application();
+    return MainApp();
   }
 }
 
-Widget application(){
-return MaterialApp(
-  home: Scaffold(
-    backgroundColor: Colors.blueGrey,
-    body: SafeArea(
+
+
+
+Widget MainApp(){
+  return MaterialApp(
+    home: Scaffold(
+      backgroundColor: Colors.blueGrey,
+      body: body(),
+    ),
+  );
+}
+
+Widget body(){
+  return SafeArea(
       child: DecoratedBox(
         position: DecorationPosition.background,
         decoration: BoxDecoration(
-       image: DecorationImage(
-         image: AssetImage('images/drum.png'),
-         fit:BoxFit.fitWidth,
-       ),
+            image: DecorationImage(
+                image: AssetImage('images/drum.png'),
+             fit: BoxFit.fitWidth
+            )
         ),
-        child: Container(
 
-        ),
-      ),
-    ),
-  ),
+child: buttons(),
+      )
 );
+}
+
+
+
+Widget buttons(){
+  return Column(
+    children: [
+      Expanded(child:  Row(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+
+          Expanded(child:TextButton(onPressed: () {
+            playAudio('c1.wav');
+          }, child: Text('1'))),
+          Expanded(child:TextButton(onPressed: () {
+            playAudio('c2.wav');
+            }, child: Text('2'))),
+        ],
+      ),),
+
+      Expanded(child:  Row(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          Expanded(child:TextButton(onPressed: () {
+            playAudio('h1.wav');
+          }, child: Text('3'))),
+          Expanded(child:TextButton(onPressed: () {
+            playAudio('h2.wav');
+          }, child: Text('4'))),
+        ],
+      ),),
+
+      Expanded(child:  Row(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          Expanded(child:TextButton(onPressed: () {
+            playAudio('k1.wav');
+          }, child: Text('5'))),
+          Expanded(child:TextButton(onPressed: () {
+            playAudio('k2.wav');
+          }, child: Text('6'))),
+
+        ],
+      ),),
+
+    ],
+  );
+}
+
+
+
+
+void playAudio(audio){
+  var player = AudioCache();
+  player.play(audio);
 }
